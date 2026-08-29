3f8d840985fba209a5d10b021e9eee0b53abbdc4
oliverduckk <48552295+oliverduckk@users.noreply.github.com>
2026-08-29 21:33:14 +1000

    Added further code so Mairon doesnt die and accepts repeated commands until the user types "exit".

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
 print("Mairon v0.1 starting...")
 input_name = input("What is your name? ")
 print(f"Good evening, {input_name}.")
+
+while True:
+    user_input = input("You: ").strip().lower()
+
+    if user_input == "exit":
+        print("Mairon: Shutting down.")
+        break
+
+    else:
+        print("Mairon: I don't understand that command yet.\n")
